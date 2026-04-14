fab6ace773a40a2039da2b9c0153f17f869dd060
ayush456aahe-netizen <ayush456aahe@gmail.com>
2026-04-14 10:21:59 +0530

    Add files via upload

---
+++ t1.js
@@ -0,0 +1,11 @@
+// Simple animation effect
+
+document.querySelectorAll(".card").forEach(card => {
+  card.addEventListener("mouseover", () => {
+    card.style.transform = "scale(1.05)";
+  });
+
+  card.addEventListener("mouseout", () => {
+    card.style.transform = "scale(1)";
+  });
+});
